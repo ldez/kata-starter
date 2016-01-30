ed78aa5f081d60087ef3c9948cbbb8e094d227e1
Fernandez Ludovic <ludovic.fernandez@zenika.com>
2016-01-30 12:22:59 +0100

    WIP: qunit karma babel

---
+++ js/with-qunit/wip-qunit-karma-babel/test/sample.test.js
@@ -0,0 +1,29 @@
+(function () {
+  'use strict';
+
+  QUnit.module("module A", {
+    beforeEach: function () {
+      // prepare something for all following tests
+    },
+    afterEach: function () {
+      // clean up after each test
+    }
+  });
+
+  QUnit.test("a basic test example", function (assert) {
+    assert.ok(true, "this test is fine");
+  });
+  QUnit.test("a basic test example 2", function (assert) {
+    assert.ok(true, "this test is fine");
+  });
+
+  QUnit.module("group b");
+
+  QUnit.test("a basic test example 3", function (assert) {
+    assert.ok(true, "this test is fine");
+  });
+  QUnit.test("a basic test example 4", function (assert) {
+    assert.ok(true, "this test is fine");
+  });
+
+})();
